4bf8ec1382b309aad37ecd585c96b2e2a067e9cc
Trelyrien <jskeen@gmail.com>
2018-05-25 08:50:09 -0400

    Changes about lots of things
    For da wife

--- js/scroll.js
+++ js/scroll.js
@@ -4,3 +4,10 @@ $(document).ready(function(){
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 700);
     });
 });
+
+$.facebox.settings.closeImage = 'images/closelabel.png'
+$.facebox.settings.loadingImage = 'images/loading.gif'
+
+jQuery(document).ready(function($) {
+  $('a[rel*=facebox]').facebox()
+});
