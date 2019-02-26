2604dcf5a2a0d909341666b9eaa31bb6c5a96b85
John Skeen <jskeen@gmail.com>
2019-02-26 14:19:20 -0500

    made some changes
    finalized

--- js/scroll.js
+++ js/scroll.js
@@ -4,10 +4,28 @@ $(document).ready(function(){
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 700);
     });
 });
+// Get the modal
+var modal = document.getElementById('myModal');
 
-$.facebox.settings.closeImage = 'images/closelabel.png'
-$.facebox.settings.loadingImage = 'images/loading.gif'
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-jQuery(document).ready(function($) {
-  $('a[rel*=facebox]').facebox()
-});
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
